ade0bd4945d288f8550a0ccae308168692833684
kurokirishima-jiejie <aijie.div@gmail.com>
2023-10-08 23:22:27 +0900

    headerのハンバーガーボタン作成

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   const ham = $('#js-hamburger'); // js-hamburgerの要素を取得し、変数hamに格納
   const nav = $('#js-nav'); // js-navの要素を取得し、変数navに格納
   ham.on('click', function () { // ハンバーガーメニューをクリックしたら
-    ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
-    nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+    ham.toggleClass('js-active'); // ハンバーガーメニューにactiveクラスを付け外し
+    nav.toggleClass('js-open'); // ナビゲーションメニューにactiveクラスを付け外し
   });
 });
