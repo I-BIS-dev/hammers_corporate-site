46e763a756b1080d4c8a40a1f40a993d980d87b2
kurokirishima-jiejie <aijie.div@gmail.com>
2023-10-09 19:34:01 +0900

    レビューの修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   const ham = $('#js-hamburger');
   const nav = $('#js-nav');
   ham.on('click', function () {
-    ham.toggleClass('js-active');
-    nav.toggleClass('js-open');
+    ham.toggleClass('--active');
+    nav.toggleClass('--open');
   });
 });
