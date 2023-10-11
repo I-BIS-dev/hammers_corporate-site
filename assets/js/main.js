5ebe6cbcbae72366ac41a9f7177b0f00b004ea0b
kurokirishima-jiejie <aijie.div@gmail.com>
2023-10-11 12:52:12 +0900

    faqsection完成

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
     ham.toggleClass('--active');
     nav.toggleClass('--open');
   });
+
+  $(".l-faq__list dd").hide();
+  $(".l-faq__list dl").on("click", function (e) {
+    $('dd', this).slideToggle('fast');
+    if ($(this).hasClass('--arrow')) {
+      $(this).removeClass('--arrow');
+    } else {
+      $(this).addClass('--arrow');
+    }
+  });
 });
