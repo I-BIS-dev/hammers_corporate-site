3796856faac25dc1d844731b3376c42f803a2442
kurokirishima-jiejie <aijie.div@gmail.com>
2023-10-12 11:12:49 +0900

    jsfadeInOutアニメーション追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@ $(document).ready(function () {
   ham.on('click', function () {
     ham.toggleClass('--active');
     nav.toggleClass('--open');
+    if (nav.hasClass('--open')) {
+      $('#js-hide').hide();
+      $('.l-feature__ctabtn').hide();
+      $('.l-surbey__ctabtn').hide();
+      $('.p-inner').hide();
+      $('.l-contact').hide();
+      $('.l-access__ctabtn').hide();
+    } else {
+      $('#js-hide').show();
+      $('.l-feature__ctabtn').show();
+      $('.l-surbey__ctabtn').show();
+      $('.p-inner').show();
+      $('.l-contact').show();
+      $('.l-access__ctabtn').show();
+    }
   });
 
   $(".l-faq__list dd").hide();
