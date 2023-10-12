8c6e3a88bb0402ce556e0cb186200715872304c7
kurokirishima-jiejie <aijie.div@gmail.com>
2023-10-12 12:27:49 +0900

    jsフェードインアウト質問

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,32 @@
 $(document).ready(function () {
-  const ham = $('#js-hamburger');
+  const ham = $('#js-hamburger')
   const nav = $('#js-nav');
   ham.on('click', function () {
     ham.toggleClass('--active');
-    nav.toggleClass('--open');
-    if (nav.hasClass('--open')) {
-      $('#js-hide').hide();
-      $('.l-feature__ctabtn').hide();
-      $('.l-surbey__ctabtn').hide();
-      $('.p-inner').hide();
-      $('.l-contact').hide();
-      $('.l-access__ctabtn').hide();
-    } else {
-      $('#js-hide').show();
-      $('.l-feature__ctabtn').show();
-      $('.l-surbey__ctabtn').show();
-      $('.p-inner').show();
-      $('.l-contact').show();
-      $('.l-access__ctabtn').show();
-    }
+    nav.fadeToggle();
   });
+
+  // const ham = $('#js-hamburger');
+  // const nav = $('#js-nav');
+  // ham.on('click', function () {
+  //   ham.toggleClass('--active');
+  //   nav.toggleClass('--open');
+  //   if (nav.hasClass('--open')) {
+  //     $('#js-hide').hide();
+  //     $('.l-feature__ctabtn').hide();
+  //     $('.l-surbey__ctabtn').hide();
+  //     $('.p-inner').hide();
+  //     $('.l-contact').hide();
+  //     $('.l-access__ctabtn').hide();
+  //   } else {
+  //     $('#js-hide').show();
+  //     $('.l-feature__ctabtn').show();
+  //     $('.l-surbey__ctabtn').show();
+  //     $('.p-inner').show();
+  //     $('.l-contact').show();
+  //     $('.l-access__ctabtn').show();
+  //   }
+  // });
 
   $(".l-faq__list dd").hide();
   $(".l-faq__list dl").on("click", function (e) {
