063bdc6f86ccd43ee7df3e0259f9399d1b983935
tsubasahara <origin.wing2@gmail.com>
2023-10-18 21:22:09 +0900

    テーマフォルダ(themes/analysis）、.wordpressファイル追加。phpファイルはテーマフォルダへ集約。

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function my_script_init() {
-  wp_enqueue_style("my", get_template_directory_uri() . "/css/common.css", array(), filemtime(get_theme_file_path('css/common.css')), "all");
-  wp_enqueue_script("my", get_template_directory_uri() . "/assets/js/main.js", array("jquery"), filemtime(get_theme_file_path('/assets/js/main.js')), true);
-}
-add_action("wp_enqueue_scripts", "my_script_init");
-
-?>
