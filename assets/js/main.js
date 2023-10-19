8d19b3903cfec51917fc0cf2a0bbe02eb785959e
山口愛介 <aijie.div@gmail.com>
2023-10-19 11:22:29 +0900

    headerとfooter共通部分の修正とカレント表示（未完成）

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,23 @@ $(document).ready(function () {
     nav.toggleClass('--open');
   });
 
+  // const links = jQuery(".l-header__nav-link");
+
+  // links.each(function () {
+  //   if (this.href === location.href) {
+  //     jQuery(this).closest(".l-header__nav-link").addClass("current");
+  //   }
+  // });
+
+  $(".l-header__nav-list .l-header__nav-item .l-header__nav-link").on("click", function (event) {
+    // カレント表示をリセット
+    $(".l-header__nav-list .l-header__nav-item .l-header__nav-link").removeClass('current');
+
+    // クリックされたリンクにカレントクラスを追加
+    $(this).addClass('current');
+  });
+
+
   $(".l-faq__list dd").hide();
   $(".l-faq__list dl").on("click", function (e) {
     $('dd', this).slideToggle('fast');
