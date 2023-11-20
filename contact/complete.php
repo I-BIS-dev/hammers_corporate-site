dcd9609fd8a057f4b3e20ee184204b406d1b6ee2
山口愛介 <aijie.div@gmail.com>
2023-11-20 15:12:09 +0900

    デザイン修正

--- contact/complete.php
+++ contact/complete.php
@@ -27,7 +27,7 @@ $message  = $_POST['name'] . " 様 \r\n"
   . "──────────────────────";
 $headers = "Content-Type: text/plain; charset=UTF-8\r\n";
 // $headers = "From: info-b@bestar7.jp";
-$headers .= "From: From: info-b@bestar7.jp\r\n";
+$headers .= "From: 株式会社ハマーズ 分析事業部 (BESTAR)\r\n";
 $headers .= "Return-Path: From: info-b@bestar7.jp\r\n";
 
 mail($to, $subject, $message, $headers);
@@ -56,7 +56,7 @@ $message  = "\r\n"
   . "＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝\r\n";
 $headers = "Content-Type: text/plain; charset=UTF-8\r\n";
 // $headers = "From: info-b@bestar7.jp";
-$headers .= "From: From: info-b@bestar7.jp\r\n";
+// $headers .= "From: 株式会社ハマーズ 分析事業部 (BESTAR)\r\n";
 $headers .= "Return-Path: From: info-b@bestar7.jp\r\n";
 
 mail($to, $subject, $message, $headers);
